9a8532163b5c6be5a1437b4b48d277e496aeac13
Jason White <jason.white@shopify.com>
2017-02-20 18:20:58 -0500

    adding mypy checking

--- tests/python_template/test_my_module.py
+++ tests/python_template/test_my_module.py
@@ -3,9 +3,9 @@ from python_template import my_module
 
 
 @pytest.fixture
-def my_object():
+def my_object() -> my_module.MyClass:
     return my_module.MyClass()
 
 
-def test_adds_one(my_object):
+def test_adds_one(my_object: my_module.MyClass):
     assert my_object.add_one(7) == 8
