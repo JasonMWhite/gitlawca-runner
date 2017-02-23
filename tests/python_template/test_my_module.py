bccc39f7b09a19a7ecc3c54d3e5099627def3654
Jason White <jason.white@shopify.com>
2017-02-23 02:30:28 -0500

    enhance my_module

--- tests/python_template/test_my_module.py
+++ tests/python_template/test_my_module.py
@@ -2,10 +2,23 @@ import pytest
 from python_template import my_module
 
 
-@pytest.fixture
-def my_object() -> my_module.MyClass:
-    return my_module.MyClass()
+class TestMyClass:
 
+    @pytest.fixture
+    def my_object(self) -> my_module.MyClass:
+        return my_module.MyClass(10)
 
-def test_adds_one(my_object: my_module.MyClass):
-    assert my_object.add_one(7) == 8
+    def test_adds_constant(self, my_object: my_module.MyClass):
+        assert my_object.add_constant(7) == 17
+
+class TestMySubClass:
+
+    @pytest.fixture
+    def my_other_object(self) -> my_module.MySubClass:
+        return my_module.MySubClass(10, 5)
+
+    def test_adds_both_values(self, my_other_object: my_module.MySubClass):
+        assert my_other_object.add_both_values() == 15
+
+    def test_adds_constant(self, my_other_object: my_module.MySubClass):
+        assert my_other_object.add_constant(2) == 12
