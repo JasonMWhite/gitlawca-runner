4d774415784e5912250b56812877b1a7effc53f6
Jason White <jason.white@shopify.com>
2017-05-17 02:22:50 -0400

    add spider middleware

--- scraper/justice/settings.py
+++ scraper/justice/settings.py
@@ -46,9 +46,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'justice.middlewares.JusticeSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scraper.justice.middlewares.JusticeSpiderMiddleware': 500,
+}
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
